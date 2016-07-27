3f7f86493415700b494cae1c26f663940d635c72
xxx <hululu@baidu.com>
2016-07-27 17:54:41 +0800

    add redex debug

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -1,40 +1,44 @@
 /**
- * # configureStore.js
- * 
- * A Redux boilerplate setup
- * 
- */
+* # configureStore.js
+*
+* A Redux boilerplate setup
+*
+*/
 'use strict';
 
 /**
- * ## Imports
- * 
- * redux functions
- */
-import { createStore, applyMiddleware } from 'redux';
+* ## Imports
+*
+* redux functions
+*/
+import { createStore, applyMiddleware,compose } from 'redux';
 import thunk from 'redux-thunk';
 
 /**
 * ## Reducer
-* The reducer contains the 4 reducers from 
+* The reducer contains the 4 reducers from
 * device, global, auth, profile
 */
 import reducer from '../reducers';
 
 /**
- * ## creatStoreWithMiddleware
- * Like the name...
- */ 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk
-)(createStore);
+* ## creatStoreWithMiddleware
+* Like the name...
+*/
 
 /**
- * ## configureStore
- * @param {Object} the state with for keys:
- * device, global, auth, profile
- * 
- */ 
-export default function configureStore(initialState) {
-  return createStoreWithMiddleware(reducer, initialState);
-};
+* ## configureStore
+* @param {Object} the state with for keys:
+* device, global, auth, profile
+*
+*/
+export default function (initialState){
+  return createStore(
+    reducer,
+    initialState,
+    compose(
+      applyMiddleware(thunk),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+  );
+}
